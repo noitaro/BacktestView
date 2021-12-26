571a9bdece85db86672551bb935731a944e4536c
noitaro <52857466+noitaro@users.noreply.github.com>
2021-12-26 22:25:32 +0900

    aaa

--- web/index.js
+++ web/index.js
@@ -1,15 +1,29 @@
 const TradingVue = window['TradingVueJs'].TradingVue;
-const LineChart = window['VueChartJs'].Line;
 
 let ohlcv_data = null;
 const innerHeightOffset = 231;
+
+Vue.component('line-chart', {
+    extends: VueChartJs.Line,
+    mounted() {
+        this.renderChart({
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'Data One',
+                    backgroundColor: '#f87979',
+                    data: [40, 39, 10, 40, 39, 80, 40]
+                }
+            ]
+        }, { responsive: true, maintainAspectRatio: false })
+    }
+});
 
 var app = new Vue({
     el: '#app',
     vuetify: new Vuetify(),
     components: {
-        'trading-vue': TradingVue,
-        'line-chart': LineChart
+        'trading-vue': TradingVue
     },
     data() {
         return {
